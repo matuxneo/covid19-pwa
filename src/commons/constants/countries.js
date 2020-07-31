9c390bca3d782f838bb0a1583507c2c7c05ca924
Matuzalém Guimarães <matuinfo@gmail.com>
2020-07-30 22:59:31 -0300

    Primeiro Comit
    Primeiro Comit deste projeto

--- src/commons/constants/countries.js
+++ src/commons/constants/countries.js
@@ -1,35 +1,40 @@
-import BrazilFlag from 'assets/images/brazil.png'
-import ChinaFlag from 'assets/images/china.png'
-import GermanyFlag from 'assets/images/germany.png'
-import ItalyFlag from 'assets/images/italy.png'
-import UsaFlag from 'assets/images/usa.png'
+import BrazilFlag from 'assets/images/brazil.png';
+import ChinaFlag from 'assets/images/china.png';
+import GermanyFlag from 'assets/images/germany.png';
+import ItalyFlag from 'assets/images/italy.png';
+import UsaFlag from 'assets/images/usa.png';
 
 const COUNTRIES = [
   {
+    label: 'Espanha',
+    value: 'spain',
+    flag: BrazilFlag,
+  },
+  {
     label: 'Alemanha',
     value: 'germany',
-    flag: GermanyFlag
+    flag: GermanyFlag,
   },
   {
     label: 'Brasil',
     value: 'brazil',
-    flag: BrazilFlag
+    flag: BrazilFlag,
   },
   {
     label: 'China',
     value: 'china',
-    flag: ChinaFlag
+    flag: ChinaFlag,
   },
   {
     label: 'Estados Unidos',
     value: 'usa',
-    flag: UsaFlag
+    flag: UsaFlag,
   },
   {
     label: 'Itália',
     value: 'italy',
-    flag: ItalyFlag
-  }
-]
+    flag: ItalyFlag,
+  },
+];
 
-export default COUNTRIES
+export default COUNTRIES;
